3c54be771ddfa9e3462683f121f7bdabef6497cc
unknown <naosyth@gmail.com>
2015-11-26 00:08:49 -0800

    Code cleanup

--- helicopter.cs
+++ helicopter.cs
@@ -40,34 +40,38 @@ Vector3 oldPos = new Vector3(0.0, 0.0, 0.0);
 // Control constants
 double MaxPitch = 67.5;
 double MaxRoll = 67.5;
-int GyroResponsiveness = 2; // Any positive non-zero integer 
+int GyroResponsiveness = 4; // Larger = more gradual angle drop
 int GyroCount = 3; // Number of gyros to use for auto hover
 
 // Blocks
-IMyRemoteControl Remote;
-IMyGyro Gyro; // This gyro is used to control enable/disable state, so that the state can be printed to the text panel
-IMyTextPanel Screen;
-List<IMyGyro> Gyros;
+IMyRemoteControl remote;
+IMyGyro gyro; // Main gyro, used to control auto hover on/off status
+IMyTextPanel screen;
+List<IMyGyro> gyros;
+
+bool Initialized = false;
 
 void Main() {
-  if (Remote == null)
+  if (!Initialized)
     Initialize();
 
   StopVehicle();
 }
 
 void Initialize() {
-  Remote = GridTerminalSystem.GetBlockWithName(RemoteControlName) as IMyRemoteControl;
-  Gyro = GridTerminalSystem.GetBlockWithName(GyroName) as IMyGyro;
+  remote = GridTerminalSystem.GetBlockWithName(RemoteControlName) as IMyRemoteControl;
+  gyro = GridTerminalSystem.GetBlockWithName(GyroName) as IMyGyro;
 
   if (!String.IsNullOrEmpty(TextPanelName))
-    Screen = GridTerminalSystem.GetBlockWithName(TextPanelName) as IMyTextPanel;
+    screen = GridTerminalSystem.GetBlockWithName(TextPanelName) as IMyTextPanel;
 
-  var l = new List<IMyTerminalBlock>();
-  GridTerminalSystem.GetBlocksOfType<IMyGyro>(l, x => x.CubeGrid == Me.CubeGrid && x != Gyro);
-  Gyros = l.ConvertAll(x => (IMyGyro)x);
-  Gyros.Insert(0, Gyro); // Make sure the master gyro is included after truncation
-  Gyros = Gyros.GetRange(0, GyroCount);
+  var list = new List<IMyTerminalBlock>();
+  GridTerminalSystem.GetBlocksOfType<IMyGyro>(list, x => x.CubeGrid == Me.CubeGrid && x != gyro);
+  gyros = list.ConvertAll(x => (IMyGyro)x);
+  gyros.Insert(0, gyro); // Make sure the master gyro is head of list
+  gyros = gyros.GetRange(0, GyroCount);
+
+  Initialized = true;
 }
 
 void StopVehicle() {
@@ -75,34 +79,35 @@ void StopVehicle() {
   double dt = 1000 / 60;
 
   // Get delta position
-  Vector3 pos = Remote.GetPosition();
+  Vector3 pos = remote.GetPosition();
   Vector3 deltaPos = pos - oldPos;
   oldPos = pos;
-
   double speed = deltaPos.Length() / dt * 1000;
 
   // Get orientation vectors
-  Matrix orientation = Remote.WorldMatrix;
-  Vector3 normalVec = orientation.Up;
+  Matrix orientation = remote.WorldMatrix;
+  Vector3 upVec = orientation.Up;
   Vector3 forwardVec = orientation.Forward;
   Vector3 rightVec = orientation.Right;
 
   // Determine speed forward and sideways in m/s
   double speedForward = Vector3.Dot(deltaPos, forwardVec) / dt * 1000;
   double speedRight = Vector3.Dot(deltaPos, rightVec) / dt * 1000;
-  double speedUp = Vector3.Dot(deltaPos, normalVec) / dt * 1000;
+  double speedUp = Vector3.Dot(deltaPos, upVec) / dt * 1000;
 
   // Get gravity vector
-  Vector3 gravityVec = -Vector3.Normalize(Remote.GetNaturalGravity());
-  double gravity = Remote.GetNaturalGravity().Length() / 9.81;
+  Vector3 gravityVec = -Vector3.Normalize(remote.GetNaturalGravity());
+  double gravity = remote.GetNaturalGravity().Length() / 9.81;
 
   if (float.IsNaN(gravityVec.GetDim(0))) {
-    if (Screen != null) {
-      Screen.WritePublicText("No Gravity" + 
-        "\n\nSpeed: " + String.Format("{0:000.0}", speed) + " m/s" +
-        "\nForward: " + String.Format("{0:000.0}", speedForward) + " m/s" +
-        "\nRight: " + String.Format("{0:000.0}", speedRight) + " m/s" +
-        "\nUp: " + String.Format("{0:000.0}", speedUp) + " m/s");
+    if (screen != null) {
+      screen.WritePublicText(
+        "> Auto Hover [No Gravity]" +
+        "\n----- Velocity ----------------------------------------" +
+        "\nTotal: " + String.Format("{0:000}", speed) + " m/s" +
+        "\n  F/B: " + String.Format("{0:000}", speedForward) +
+        "\n  R/L: " + String.Format("{0:000}", speedRight) +
+        "\n  U/D: " + String.Format("{0:000}", speedUp));
     }
     return;
   }
@@ -111,42 +116,47 @@ void StopVehicle() {
   double pitch = Vector3.Dot(gravityVec, forwardVec) / (gravityVec.Length() * forwardVec.Length()) * 90;
   double roll = Vector3.Dot(gravityVec, rightVec) / (gravityVec.Length() * rightVec.Length()) * 90;
 
-  // Scale max pitch based on speed
+  // Scale desired pitch based on speed
   double desiredPitch = Math.Atan(speedForward / GyroResponsiveness) / (Math.PI / 2) * MaxPitch;
   double desiredRoll = Math.Atan(speedRight / GyroResponsiveness) / (Math.PI / 2) * MaxRoll;
 
-  float pitchRate = (float) (Gyro.GetMaximum<float>("Pitch") * (desiredPitch - pitch) /  MaxPitch);
-  float rollRate = (float) (Gyro.GetMaximum<float>("Roll") * (desiredRoll - roll) /  MaxRoll);
-  Vector3 rotationVec = new Vector3(pitchRate, 0, rollRate);
+  // Scale gyro rate based on difference bewteen the current and desired angle
+  float pitchRate = (float)(gyro.GetMaximum<float>("Pitch") * (desiredPitch - pitch) /  MaxPitch);
+  float rollRate = (float)(gyro.GetMaximum<float>("Roll") * (desiredRoll - roll) /  MaxRoll);
 
   // Transform rotation to match the remote control block's orientation rather than the "build" orientation
   Matrix shipOrientation;
-  Remote.Orientation.GetMatrix(out shipOrientation);
+  remote.Orientation.GetMatrix(out shipOrientation);
+  Vector3 rotationVec = new Vector3(pitchRate, 0, rollRate);
   rotationVec = Vector3.Transform(rotationVec, shipOrientation);
 
-  Matrix localOrientation;
-  for (int i = 0; i < Gyros.Count; i++) {
-    var g = Gyros[i];
+  for (int i = 0; i < gyros.Count; i++) {
+    var g = gyros[i];
+
+    // Adjust rotation for the gyro's local orientation
+    Matrix localOrientation;
     g.Orientation.GetMatrix(out localOrientation);
+    var localRot = Vector3.Transform(rotationVec, MatrixD.Transpose(localOrientation));
 
-    var rotVec = Vector3.Transform(rotationVec, MatrixD.Transpose(localOrientation));
-
-    Gyros[i].SetValueFloat("Pitch", (float)rotVec.GetDim(0));
-    Gyros[i].SetValueFloat("Yaw", (float)-rotVec.GetDim(1));
-    Gyros[i].SetValueFloat("Roll", (float)-rotVec.GetDim(2));
-    Gyros[i].SetValueBool("Override", Gyro.GyroOverride);
+    g.SetValueFloat("Pitch", (float)localRot.GetDim(0));
+    g.SetValueFloat("Yaw", (float)-localRot.GetDim(1));
+    g.SetValueFloat("Roll", (float)-localRot.GetDim(2));
+    g.SetValueBool("Override", gyro.GyroOverride);
   }
 
   // Print status
-  if (Screen != null) {
-    Screen.WritePublicText(
-      "Speed: " + String.Format("{0:000.0}", speed) + " m/s" +
-      "\nForward: " + String.Format("{0:000.0}", speedForward) + " m/s" +
-      "\nRight: " + String.Format("{0:000.0}", speedRight) + " m/s" +
-      "\nUp: " + String.Format("{0:000.0}", speedUp) + " m/s" +
-      "\n\nRoll: " + String.Format("{0:0}", roll) + " degrees" +
-      "\nPitch: " + String.Format("{0:0}", pitch) + " degrees" +
-      "\n\nGravity: " + String.Format("{0:0.00}", gravity) + " g" +
-      "\n\nAuto Braking: " + (Gyro.GyroOverride ? "Enabled" : "Disabled"));
+  if (screen != null) {
+    screen.WritePublicText(
+      "> Auto Hover [" + (gyro.GyroOverride ? "Active" : "Disabled") + "]" +
+      "\n----- Velocity ----------------------------------------" +
+      "\nTotal: " + String.Format("{0:000}", speed) + " m/s" +
+      "\n  F/B: " + String.Format("{0:000}", speedForward) +
+      "\n  R/L: " + String.Format("{0:000}", speedRight) +
+      "\n  U/D: " + String.Format("{0:000}", speedUp) +
+      "\n\n----- Orientation ----------------------------------------" +
+      "\nPitch: " + String.Format("{0:00}", pitch) + "°" +
+      " | Roll: " + String.Format("{0:00}", roll) + "°" +
+      "\n\n----- Status ----------------------------------------" +
+      "\nGravity: " + String.Format("{0:0.00}", gravity) + " g");
   }
 }
