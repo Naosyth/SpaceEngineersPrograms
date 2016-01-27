f02a85a20f2f7b84f2115d360dfac1c7ae42abbf
unknown <naosyth@gmail.com>
2016-01-26 18:11:49 -0800

    Added space mode

--- helicopter.cs
+++ helicopter.cs
@@ -1,5 +1,5 @@
-// Helicopter Braking Script
-// Brings a planetary flying vehicle that only has upward thrust to a stop
+// Flight Assist
+// Control your spaceships without thrusters in every direction
 //
 // Author: Naosyth
 // naosyth@gmail.com
@@ -17,15 +17,15 @@
 //   This controls the curve of the angle the ship takes while coming to a stop.
 //   The curve looks something like this:
 //   http://www.wolframalpha.com/input/?i=arctan%28x%2F4%29+%2F+%283.14159+%2F+2%29+for+x+from+-104+to+104
-//   Where 4, in the above equasion, is GyroResponsiveness
+//   Where 4, in the above equation, is GyroResponsiveness
 //   Increasing GyroResponsiveness makes the angle start approaching zero earlier
 //   This can help to reduce wobble, but also means you will take a bit longer to slow down.
 //
 
 // Block Names
-static string GyroName = "AH Gyro";
-static string RemoteControlName = "AH Remote";
-static string TextPanelName = "AH Screen";
+static string GyroName = "FA Gyro";
+static string RemoteControlName = "FA Remote";
+static string TextPanelName = "FA Screen";
 
 // Program options
 static int ScreenRefreshRate = 5; // How many times per second to redraw the screen
@@ -35,7 +35,7 @@ static bool AlwaysEnabledInGravity = false;
 static double MaxPitch = 67.5;
 static double MaxRoll = 67.5;
 static int GyroResponsiveness = 8; // Larger = more gradual angle drop
-static int GyroCount = 3; // Number of gyros to use for auto hover
+static int GyroCount = 1; // Number of gyros to use for auto hover
 static double minRPM = 0.015; // Min RPM setting for gyros. Values that are too low behave weird. You shouldn't need to adjust this.
 
 const double HalfPi = Math.PI / 2;
@@ -73,6 +73,7 @@ class AutoHoverController {
 
   private Vector3 pos;
   private Vector3 oldPos;
+  private Vector3 deltaPos;
 
   private double speed;
   private double speedForward;
@@ -81,10 +82,12 @@ class AutoHoverController {
   private double controlSpeedForward;
   private double controlSpeedRight;
 
+  private Matrix orientation;
   private Vector3 forwardVec;
   private Vector3 rightVec;
   private Vector3 upVec;
 
+  private bool switchingGravity;
   private bool inGravity;
   private double gravity;
 
@@ -124,18 +127,25 @@ class AutoHoverController {
   public void Tick() {
     CalculateOrientation();
     CalculateSpeed();
-
-    if (!inGravity && gyro.GyroOverride)
-      ToggleGyros(false);
-    else if (AlwaysEnabledInGravity && inGravity && !gyro.GyroOverride)
-      ToggleGyros(true);
-    else
-      AdjustOrientation();
+    tickCount += 1;
 
     if (tickCount % (60 / ScreenRefreshRate) == 0)
       PrintStatus();
 
-    tickCount += 1;
+    // Automatically enable if AlwaysEnabledInGravity = true and in gravity wih script disabled
+    if (AlwaysEnabledInGravity && inGravity && !gyro.GyroOverride)
+      ToggleGyros(true);
+
+    if (switchingGravity)
+      ToggleGyros(false);
+
+    if (!gyro.GyroOverride)
+      return;
+    
+    if (inGravity)
+      PlanetaryManeuvers();
+    else
+      SpaceManeuvers();
   }
 
   private void CalculateSpeed() {
@@ -144,15 +154,22 @@ class AutoHoverController {
 
     // Get delta position
     pos = remote.GetPosition();
-    Vector3 deltaPos = pos - oldPos;
+    deltaPos = pos - oldPos;
     oldPos = pos;
     speed = deltaPos.Length() / dt * 1000;
 
     // Get orientation vectors
-    Matrix orientation = remote.WorldMatrix;
-    forwardVec = orientation.Forward;
-    rightVec = orientation.Right;
-    upVec = orientation.Up;
+    orientation = remote.WorldMatrix;
+
+    if (inGravity) {
+      forwardVec = orientation.Forward;
+      rightVec = orientation.Right;
+      upVec = orientation.Up;
+    } else {
+      forwardVec = orientation.Up;
+      rightVec = orientation.Right;
+      upVec = orientation.Forward;
+    }
 
     // Determine speed forward and sideways in m/s
     if (inGravity) {
@@ -161,9 +178,9 @@ class AutoHoverController {
       speedRight = Vector3.Dot(deltaPos, -Vector3.Cross(gravityVec, forwardVec)) / dt * 1000;
       speedUp = Vector3.Dot(deltaPos, gravityVec) / dt * 1000;
     } else {
-      speedForward = Vector3.Dot(deltaPos, forwardVec) / dt * 1000;
+      speedForward = Vector3.Dot(deltaPos, upVec) / dt * 1000;
       speedRight = Vector3.Dot(deltaPos, rightVec) / dt * 1000;
-      speedUp = Vector3.Dot(deltaPos, upVec) / dt * 1000;
+      speedUp = Vector3.Dot(deltaPos, forwardVec) / dt * 1000;
     }
   }
 
@@ -171,16 +188,25 @@ class AutoHoverController {
     // Get gravity vector
     Vector3 gravityVec = -Vector3.Normalize(remote.GetNaturalGravity());
     gravity = remote.GetNaturalGravity().Length() / 9.81;
+    switchingGravity = inGravity;
     inGravity = !float.IsNaN(gravityVec.GetDim(0));
+    switchingGravity = inGravity != switchingGravity;
 
     // Determine roll and pitch in degrees
     if (inGravity) {
       pitch = Vector3.Dot(gravityVec, forwardVec) / (gravityVec.Length() * forwardVec.Length()) * 90;
       roll = Vector3.Dot(gravityVec, rightVec) / (gravityVec.Length() * rightVec.Length()) * 90;
+    } else {
+      pitch = Vector3.Dot(deltaPos, forwardVec) / (deltaPos.Length() * forwardVec.Length()) * 90;
+      roll = Vector3.Dot(deltaPos, rightVec) / (deltaPos.Length() * rightVec.Length()) * 90;
+      if (speed == 0) {
+        roll = 0;
+        pitch = 0;
+      }
     }
   }
 
-  private void AdjustOrientation() {
+  private void PlanetaryManeuvers() {
     switch (mode.ToLower()) {
       case "glide":
         desiredPitch = 0;
@@ -225,35 +251,48 @@ class AutoHoverController {
     // Transform rotation to match the remote control block's orientation rather than the "build" orientation
     Matrix shipOrientation;
     remote.Orientation.GetMatrix(out shipOrientation);
+    
     Vector3 rotationVec = new Vector3(pitchRate, 0, rollRate);
     rotationVec = Vector3.Transform(rotationVec, shipOrientation);
+    SetGyros(rotationVec, gyro.GyroOverride);
+  }
 
-    for (int i = 0; i < gyros.Count; i++) {
-      var g = gyros[i];
+  private void SpaceManeuvers() {
+    pitchRate = (float)(gyro.GetMaximum<float>("Pitch") * -pitch / 90);
+    if (pitchRate > -minRPM && pitchRate < minRPM)
+      pitchRate = (float)(Math.Sign(pitchRate) * minRPM);
 
-      // Adjust rotation for the gyro's local orientation
-      Matrix localOrientation;
-      g.Orientation.GetMatrix(out localOrientation);
-      var localRot = Vector3.Transform(rotationVec, MatrixD.Transpose(localOrientation));
+    rollRate = (float)(gyro.GetMaximum<float>("Roll") * -roll / 90);
+    if (rollRate > -minRPM && rollRate < minRPM)
+      rollRate = (float)(Math.Sign(rollRate) * minRPM);
 
-      g.SetValueFloat("Pitch", (float)localRot.GetDim(0));
-      g.SetValueFloat("Yaw", (float)-localRot.GetDim(1));
-      g.SetValueFloat("Roll", (float)-localRot.GetDim(2));
-      g.SetValueBool("Override", gyro.GyroOverride);
+    if (speed < 0.5) {
+      rollRate = 0;
+      pitchRate = 0;
     }
+
+    // Transform rotation to match the remote control block's orientation rather than the "build" orientation
+    Matrix shipOrientation;
+    remote.Orientation.GetMatrix(out shipOrientation);
+
+    Vector3 rotationVec = new Vector3(pitchRate, -rollRate, 0);
+    rotationVec = Vector3.Transform(rotationVec, shipOrientation);
+    SetGyros(rotationVec, gyro.GyroOverride);
   }
 
   private void PrintStatus() {
     if (screen != null) {
       if (!inGravity) {
         screen.WritePublicText(
-          "> Auto Hover [No Gravity]" +
+          "> Auto Hover [" + (gyro.GyroOverride ? "Active" : "Disabled") + "] [Space]" +
           "\n----- Velocity ----------------------------------------" +
           "\nTotal: " + String.Format("{0:000}", speed) + " m/s" +
           "\n  F/B: " + String.Format("{0:000}", speedForward) +
           "\n  R/L: " + String.Format("{0:000}", speedRight) +
-          "\n  U/D: " + String.Format("{0:000}", speedUp));
-
+          "\n  U/D: " + String.Format("{0:000}", speedUp) +
+          "\n\n----- Orientation ----------------------------------------" +
+          "\nPitch: " + String.Format("{0:00}", pitch) + "°" +
+          " | Yaw: " + String.Format("{0:00}", roll) + "°");
         return;
       }
 
@@ -275,5 +314,21 @@ class AutoHoverController {
   private void ToggleGyros(bool state) {
     for (int i = 0; i < gyros.Count; i++)
       gyros[i].SetValueBool("Override", state);
+  }
+
+  private void SetGyros(Vector3 rotationVector, bool overrideEnabled) {
+    for (int i = 0; i < gyros.Count; i++) {
+      var g = gyros[i];
+
+      // Adjust rotation for the gyro's local orientation
+      Matrix localOrientation;
+      g.Orientation.GetMatrix(out localOrientation);
+      var localRot = Vector3.Transform(rotationVector, MatrixD.Transpose(localOrientation));
+
+      g.SetValueFloat("Pitch", (float)localRot.GetDim(0));
+      g.SetValueFloat("Yaw", (float)-localRot.GetDim(1));
+      g.SetValueFloat("Roll", (float)-localRot.GetDim(2));
+      g.SetValueBool("Override", gyro.GyroOverride);
+    }
   }
 }
